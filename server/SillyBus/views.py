10e9aad76ad935bfe7f2baba9143eb32faff5542
j-dewey <jcdewey@wm.edu>
2025-03-29 12:58:43 -0400

    Started File handling

--- server/SillyBus/views.py
+++ server/SillyBus/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
 from django.views.generic.base import TemplateResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import FileUploadForm
 
 def root(request):
     cntxt = {}
@@ -9,6 +11,11 @@ def happy_upload(request):
     cntxt = {}
     return TemplateResponse(request, 'index.html', cntxt)
 
+@csrf_exempt
 def file_upload(request):
-    print(request)
+    if request.method == "POST":
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("Handling file upload...")
+
     return happy_upload(request)
