fc4aaac89323263ae8e1f0c2aeb1c75d7060646b
j-dewey <jcdewey@wm.edu>
2025-03-30 01:09:43 -0400

    Fixed typo

--- server/SillyBus/g_calendar.py
+++ server/SillyBus/g_calendar.py
@@ -19,15 +19,6 @@ def format_date(date: str) -> tuple[str, str]:
     return (f"2025-{month_day}T00:00:00-00:00", f"2025-{month_day}23:59:59+59:59")
 
 def load_to_calendar(to_load: dict[str, Any], user_data):
-    '''
-    # make a temporary file to load from since Credentials can't accept
-    # raw str
-    temp_dir = tempfile.mkdtemp()
-    temp_file_path = os.path.join(temp_dir, "user.json")
-    with open(temp_file_path, "w") as temp_file:
-        json.dump(user_data, temp_file)
-    '''
-
     creds = Credentials(token=user_data)
     service = build('tasks', 'v1', credentials=creds)
     course: str = to_load['parent']
@@ -36,4 +27,4 @@ def load_to_calendar(to_load: dict[str, Any], user_data):
     for t in assignments:
         name = course + ' / ' + t['title']
         due = format_date(t['dueDate'])
-        print(f"t: {title}, due: {due}")
+        print(f"t: {name}, due: {due}")
